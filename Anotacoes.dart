805a8385e68741eaafd0e1aeda8f85be4813fbf7
talecco <othalesborges@gmail.com>
2023-06-27 00:08:20 -0300

    atualizacoes

--- Anotacoes.dart
+++ Anotacoes.dart
@@ -58,7 +58,7 @@ OPERADORES DE ATRIBUIÇÃO
 //numero = numero + 10; printa 20
 //numero += 10; printa 20 tbm. (-* tbm funciona)
 
-//?? "se for um valor null colocar outro valo" 
+//?? "se for um valor null colocar outro valor" 
 //ex: 
 
 var numero = getNumero();
@@ -116,3 +116,46 @@ final resultado = conteudo ? 'publico' : 'privado';
 print(resultado);
 
 // retornará publico, se conteudo = false, retornara privado. 
+
+RESUMO DE OPERADORES 
+
+>=	Maior ou igual
+>	Maior
+<=	Menor ou igual
+<	Menor
+is	É do mesmo tipo
+is!	Não é do mesmo tipo
+==	Igual
+!=	Diferente
+&&	E lógico (AND)
+||	OU lógico (OR)
+
+
+NUMEROS 
+
+abs()	Retorna o valor absoluto do número.
+ceil()	Retorna o último inteiro imediatamente superior.
+ceilToDouble()	Retorna o último número imediatamente superior com o tipo double.
+clamp(num limiteInferior, num limiteSuperior)	Se o número estiver dentro do limite, retorna o número. Se não, retorna o limite o qual ele extrapolou.
+compareTo(num outro)	Compara com outro número, retornando 1 quando forem diferentes e 0 quando forem iguais.
+floor()	Arredonda o número para o inteiro anterior.
+
+EX:
+double pi = 3.14159265359;
+
+int pi_arredondado = pi.floor();
+
+print(pi_arredondado); // 3
+
+floorToDouble()	Arredonda o número para o número inteiro anterior no tipo double.
+remainder(num outro)	Retorna a sobra da divisão com outro número.
+round()	Arredonda o número para o inteiro mais próximo.
+roundToDouble()	Arredonda o número para o valor inteiro mais próximo no tipo double.
+toDouble()	Converte o número para Double.
+toInt()	Converte o número para Int.
+toString()	Converte o número em uma String.
+toStringAsExponential([int digitos])	Converte para string com exponencial.
+toStringAsFixed(int decimais)	Converte para String contendo N casas decimais.
+toStringAsPrecision(int digitos)	Converte para String contendo N dígitos.
+truncate()	Retira as casas decimais, retornando um inteiro.
+truncateToDouble()	Retira as casas decimais, retornando um double.
